fd7f535c703b11d9dcb021b9aaff57a5e1a106d2
xzj <1638492106@qq.com>
2023-05-18 17:50:42 +0800

    first commit

---
+++ src/admin/app.example.js
@@ -0,0 +1,46 @@
+/*
+ * @Author       : xzj 
+ * @Date         : 2023-05-18 17:19:01
+ * @LastEditors  : xzj 
+ * @LastEditTime : 2023-05-18 17:40:42
+ * @Description  : 
+ */
+const config = {
+  locales: [
+    // 'ar',
+    // 'fr',
+    // 'cs',
+    // 'de',
+    // 'dk',
+    // 'es',
+    // 'he',
+    // 'id',
+    // 'it',
+    // 'ja',
+    // 'ko',
+    // 'ms',
+    // 'nl',
+    // 'no',
+    // 'pl',
+    // 'pt-BR',
+    // 'pt',
+    // 'ru',
+    // 'sk',
+    // 'sv',
+    // 'th',
+    // 'tr',
+    // 'uk',
+    // 'vi',
+    'zh-Hans',
+    'zh',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
---
+++ src/admin/app.js
@@ -0,0 +1,28 @@
+/*
+ * @Author       : xzj 
+ * @Date         : 2023-05-18 17:43:04
+ * @LastEditors  : xzj 
+ * @LastEditTime : 2023-05-18 17:47:19
+ * @Description  : 
+ */
+/*
+ * @Author       : xzj 
+ * @Date         : 2023-05-18 17:19:01
+ * @LastEditors  : xzj 
+ * @LastEditTime : 2023-05-18 17:40:42
+ * @Description  : 
+ */
+const config = {
+  locales: [
+    'zh-Hans',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
